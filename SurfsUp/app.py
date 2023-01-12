e12f7692e808fa5d06c373525523d45d3ac1b89f
gnimeth <114787248+gnimeth@users.noreply.github.com>
2023-01-12 07:52:59 -0600

    upload

---
+++ SurfsUp/app.py
@@ -0,0 +1,163 @@
+import numpy as np
+
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, desc
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f'Available Routes: <br/>'
+        f'/api/v1.0/precipitation <br/>'
+        f'/api/v1.0/stations <br/>'
+        f'/api/v1.0/tobs <br/>'
+        f'/api/v1.0/start_date  <br/>'
+        f'/api/v1.0/start_date/end_date <br/>'
+        f'Note start_date and end_date must be in yyyy-mm-dd format <br/>'
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return Last 12 months of precipitation data"""
+    
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    results = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= query_date).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    precp = list(np.ravel(results))
+    return jsonify(precp)
+
+@app.route('/api/v1.0/stations')
+def stations():
+
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+    
+    """Return list of stations"""
+    results = session.query(station.station).all()
+
+    session.close()
+    
+    stations_list = list(np.ravel(results))
+    return jsonify(stations_list)
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    """Temperature observations of the most active station for the past year"""
+
+    query_date = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    results = session.query(measurement.date, measurement.tobs).\
+    filter(measurement.station =='USC00519281').\
+    filter(measurement.date >= query_date).all()
+
+    session.close()
+
+    year_data = list(np.ravel(results))
+    return jsonify(year_data)
+
+@app.route('/api/v1.0/<start_date>')
+def start(start_date):
+    session = Session(engine)
+    """Return summary statistics of temperature after start date"""
+    
+    start_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
+   
+
+    results = session.query(func.min(measurement.tobs),
+              func.max(measurement.tobs),
+              func.avg(measurement.tobs)).\
+              filter((measurement.date) >= start_date).all()
+
+    session.close()
+
+    stats_list = []
+    for min, avg, max in results:
+        stat_dict = {}
+        stat_dict["Start Date"] = start_date
+        stat_dict["TMIN"] = min
+        stat_dict["TAVG"] = avg
+        stat_dict["TMAX"] = max
+        stats_list.append(stat_dict)
+
+    #print results or error message
+    if stats_list: 
+        return jsonify(stats_list)
+    else:
+        return jsonify({"error": f"check format or date not found"}), 404
+    
+
+@app.route('/api/v1.0/<start_date>/<end_date>')
+def summary_between(start_date, end_date):
+    session = Session(engine)
+    """Return summary statistics of temperature between the start and end date"""
+
+    start_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end_date, "%Y-%m-%d")
+    
+    results = session.query(func.min(measurement.tobs),
+              func.max(measurement.tobs),
+              func.avg(measurement.tobs)).\
+              filter((measurement.date) >= start_date).\
+              filter((measurement.date) <= end_date).all()
+
+    session.close()
+    
+    stats_list = []
+    for min, avg, max in results:
+        stat_dict = {}
+        stat_dict["Start Date"] = start_date
+        stat_dict["End Date"] = end_date
+        stat_dict["TMIN"] = min
+        stat_dict["TAVG"] = avg
+        stat_dict["TMAX"] = max
+        stats_list.append(stat_dict)
+
+    #print results or error message
+    if stats_list: 
+        return jsonify(stats_list)
+    else:
+        return jsonify({"error": f"check format or date not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
